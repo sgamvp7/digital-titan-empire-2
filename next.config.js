741b8a43e9ee7d3e780e85a915839ba631aa28c5
sgamvp7 <sgamvp7@gmail.com>
2025-08-06 10:26:17 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_YOUTUBE_API_KEY: "AIzaSyANXwPAuoX6FolkFs7SglWPAz6pA2XpbPI",
+  },
 };
 
 module.exports = nextConfig;
