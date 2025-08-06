92a073217e877242390ea7c87cc54ef94c68215c
sgamvp7 <sgamvp7@gmail.com>
2025-08-06 08:34:05 -0400

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🔥 The Digital Titan Empire is Alive 🔥</h1>
+      <p>Welcome to the most powerful website in history.</p>
+    </div>
+  );
+}
